9dc38497573c4653f76d6960f15bfb462ac5f21d
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-10-25 08:51:01 +0500

    Scaffold, TopAppBar, BottomNavigation

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,16 +24,82 @@ class MainActivity : ComponentActivity() {
         setContent {
             VkNewsClientTheme {
                 // A surface container using the 'background' color from the theme
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                        .padding(8.dp)
-                ) {
-                    PostCard()
-                }
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(MaterialTheme.colors.background)
+//                        .padding(8.dp)
+//                ) {
+//                    PostCard()
+//                }
+                Test()
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun Test() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "TopAppBar title")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    },
+                    label = {
+                        Text(text = "Favourite")
+                    }
+                )
+                BottomNavigationItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Outlined.Edit, contentDescription = null)
+                    },
+                    label = {
+                        Text(text = "Favourite")
+                    }
+                )
+                BottomNavigationItem(
+                    selected = true,
+                    onClick = {},
+                    icon = {
+                        Icon(Icons.Outlined.Delete, contentDescription = null)
+                    },
+                    label = {
+                        Text(text = "Favourite")
+                    }
+                )
+            }
+        },
+        drawerContent = {
+            Text(text = "Text1")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text3")
+        }
+    ) {
+        Text(
+            modifier = Modifier.padding(it),
+            text = "This is scaffold content"
+        )
     }
 }
 
