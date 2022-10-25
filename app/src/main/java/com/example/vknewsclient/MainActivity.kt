39497b58871450b5d6a49dca40dfe19b78a7f304
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-10-25 09:18:37 +0500

    Added MainScreen

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.vknewsclient.ui.theme.MainScreen
 import com.example.vknewsclient.ui.theme.PostCard
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -23,83 +24,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                // A surface container using the 'background' color from the theme
-//                Box(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(MaterialTheme.colors.background)
-//                        .padding(8.dp)
-//                ) {
-//                    PostCard()
-//                }
-                Test()
+                MainScreen()
             }
         }
-    }
-}
-
-@Preview
-@Composable
-private fun Test() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "TopAppBar title")
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(Icons.Filled.Menu, contentDescription = null)
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            BottomNavigation {
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Favourite")
-                    }
-                )
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Outlined.Edit, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Favourite")
-                    }
-                )
-                BottomNavigationItem(
-                    selected = true,
-                    onClick = {},
-                    icon = {
-                        Icon(Icons.Outlined.Delete, contentDescription = null)
-                    },
-                    label = {
-                        Text(text = "Favourite")
-                    }
-                )
-            }
-        },
-        drawerContent = {
-            Text(text = "Text1")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Text2")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Text3")
-        }
-    ) {
-        Text(
-            modifier = Modifier.padding(it),
-            text = "This is scaffold content"
-        )
     }
 }
 
