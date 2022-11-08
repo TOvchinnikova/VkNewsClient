d9b1bb6c1da236fab786b1f8807d4da720f4a05d
t.ovchinnikova <t.ovchinnikova@evotor.ru>
2022-11-08 08:44:07 +0500

    Added LazyColumn, SwipeToDismiss

--- app/src/main/java/com/example/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/MainViewModel.kt
@@ -8,11 +8,17 @@ import com.example.vknewsclient.domain.StatisticItem
 
 class MainViewModel : ViewModel() {
 
-    private val _feedPost = MutableLiveData(FeedPost())
-    val feedPost: LiveData<FeedPost> = _feedPost
+    private val sourceList = mutableListOf<FeedPost>().apply {
+        repeat(10) {
+            add(FeedPost(id = it))
+        }
+    }
+    private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
+    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
 
-    fun updateCount(statisticItem: StatisticItem) {
-        val oldStatistics = feedPost.value?.statistics ?: throw IllegalStateException()
+    fun updateCount(feedPost: FeedPost, statisticItem: StatisticItem) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldStatistics = feedPost.statistics
         val newStatistics = oldStatistics.toMutableList().apply {
             replaceAll { oldItem ->
                 if (oldItem.type == statisticItem.type) {
@@ -24,6 +30,21 @@ class MainViewModel : ViewModel() {
                 }
             }
         }
-        _feedPost.value = _feedPost.value?.copy(statistics = newStatistics)
+        val newFeedPost = feedPost.copy(statistics = newStatistics)
+        _feedPosts.value = oldPosts.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) {
+                    newFeedPost
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    fun remove(feedPost: FeedPost) {
+        val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
+        oldPosts.remove(feedPost)
+        _feedPosts.value = oldPosts
     }
 }
--- app/src/main/java/com/example/vknewsclient/domain/FeedPost.kt
+++ app/src/main/java/com/example/vknewsclient/domain/FeedPost.kt
@@ -3,6 +3,7 @@ package com.example.vknewsclient.domain
 import com.example.vknewsclient.R
 
 data class FeedPost(
+    val id: Int = 0,
     val communityName: String = "/dev/null",
     val publicationDate: String = "14:00",
     val avatarResId: Int = R.drawable.post_comunity_thumbnail,
